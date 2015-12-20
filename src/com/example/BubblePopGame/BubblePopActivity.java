7b05404932786ba7d087d0ca26af89d0e8078971
rumshenoy <rumshenoy@gmail.com>
2015-12-19 21:43:09 -0800

    Initial skeleton code

---
+++ src/com/example/BubblePopGame/BubblePopActivity.java
@@ -0,0 +1,15 @@
+package com.example.BubblePopGame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BubblePopActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
