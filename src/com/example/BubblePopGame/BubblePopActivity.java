d2b12c42a7fe32d3d89bb8994233eee7ac1f5783
rumshenoy <rumshenoy@gmail.com>
2015-12-19 21:54:18 -0800

    Setting Audio Manager and Basic Layout

--- src/com/example/BubblePopGame/BubblePopActivity.java
+++ src/com/example/BubblePopGame/BubblePopActivity.java
@@ -1,15 +1,35 @@
 package com.example.BubblePopGame;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class BubblePopActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+
+    private RelativeLayout mainFrame;
+    private Bitmap mBitMap;
+    private AudioManager mAudioManager;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        mainFrame = (RelativeLayout) findViewById(R.id.frame);
+        mBitMap = BitmapFactory.decodeResource(getResources(), R.drawable.b64);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        
+
     }
 }
