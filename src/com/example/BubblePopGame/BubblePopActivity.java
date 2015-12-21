4893e140dbdea6f0243b0e2fa1f532e7ad8e83c4
rumshenoy <rumshenoy@gmail.com>
2015-12-20 18:39:49 -0800

    Completing the BubbleView with functionality for start, stop, intersect etc

--- src/com/example/BubblePopGame/BubblePopActivity.java
+++ src/com/example/BubblePopGame/BubblePopActivity.java
@@ -4,16 +4,17 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.util.AttributeSet;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.View;
+import android.view.*;
 import android.widget.RelativeLayout;
 
 import java.util.Random;
+import java.util.concurrent.*;
 
 public class BubblePopActivity extends Activity {
     /**
@@ -32,6 +33,10 @@ public class BubblePopActivity extends Activity {
     private  int mSoundID;
     private SoundPool mSoundPool;
     private GestureDetector mGestureDetector;
+
+    private static final int MENU_STILL = Menu.FIRST;
+    private static final int MENU_SINGLE_SPEED = Menu.FIRST + 1;
+    private static final int MENU_RANDOM_SPEED = Menu.FIRST + 2;
 
     private int mDisplayWidth, mDisplayHeight;
 
@@ -128,6 +133,9 @@ public class BubblePopActivity extends Activity {
         private static final int REFRESH_RATE = 40;
         private int mScaledBitmapWidth;
         private  Bitmap mScaledBitmap;
+        private final Paint mPaint = new Paint();
+        private ScheduledFuture<?> mMoverFuture;
+
 
         private float mXPos, mYPos, mDx, mDy;
         private long mRotate, mDRotate;
@@ -138,31 +146,178 @@ public class BubblePopActivity extends Activity {
             Random r = new Random();
 
             createScaledBitmap(r);
+
+            mXPos = x - mScaledBitmapWidth/2;
+            mYPos = y - mScaledBitmapWidth/2;
+
+            setSpeedAndDirection(r);
+
+            setRotation(r);
+
+            mPaint.setAntiAlias(true);
+        }
+
+        private void setRotation(Random r) {
+            if(speedMode == RANDOM){
+                //rotation speed [1..3]
+                mDRotate = r.nextInt(3) + 1; // [r.nextInt(high-low+1) + low] if high is inclusive
+            }else{
+                mDRotate = 0;
+            }
+        }
+
+        private void setSpeedAndDirection(Random r) {
+            switch (speedMode){
+                case SINGLE:
+                    //fixed
+
+                    mDx =10;
+                    mDy = 10;
+                    break;
+
+                case STILL:
+                    mDx = 0;
+                    mDy = 0;
+                    break;
+
+                default:
+                    // Limit movement speed in the x and y
+                    // direction to [-3..3].
+
+                    //// (3 - (-3) + 1)
+                    mDx = r.nextInt(7) - 3;
+                    mDy = r.nextInt(7) - 3;
+                    break;
+            }
         }
 
         private void createScaledBitmap(Random r) {
             if(speedMode == RANDOM){
-
+                mScaledBitmapWidth = BITMAP_SIZE *3;
             }else{
-
+                // set scaled bitmap size in range [1..3] * BITMAP_SIZE
+                mScaledBitmapWidth = (r.nextInt(3) +1 ) * BITMAP_SIZE;
             }
+
+            this.mScaledBitmap = Bitmap.createScaledBitmap(mBitMap, mScaledBitmapWidth, mScaledBitmapWidth, false);
         }
 
-        public boolean intersects(float rawX, float rawY) {
-            return false;
+        public boolean intersects(float x, float y) {
+            //Return true if the BubbleView intersects position (x,y)
+            // To do this as a circle instead of a rectangle
+
+            final int radius = (mScaledBitmapWidth/2);
+            final double deltaX = (mXPos + radius) - x;
+            final double deltaY = (mYPos + radius) - y;
+            final double distance = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
+            return distance <=radius;
         }
 
         public void stop(boolean poppedByUser) {
 
+            if(mMoverFuture != null && mMoverFuture.cancel(true)){
+                final BubbleView childView= this;
+                mainFrame.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mainFrame.removeView(childView);
+                        if(poppedByUser){
+                            mSoundPool.play(mSoundID, mStreamVolume, mStreamVolume, 1, 0, 1.0f);
+                        }
+                    }
+                });
+            }
         }
 
         public void start() {
+            //creating a worker thread
+            ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
 
+            // Execute the run() in Worker Thread every REFRESH_RATE
+            // milliseconds
+            // Save reference to this job in mMoverFuture
+            mMoverFuture = executorService.scheduleWithFixedDelay(new Runnable() {
+                @Override
+                public void run() {
+                    // Each time this method is run the BubbleView should
+                    // move one step. If the BubbleView exits the display,
+                    // stop the BubbleView's Worker Thread.
+                    // Otherwise, request that the BubbleView be redrawn.
 
+                    if(BubbleView.this.moveWhileOnScreen()){
+                        BubbleView.this.stop(false);
+                    }else{
+                        BubbleView.this.postInvalidate();
+                    }
+                }
+            }, 0, REFRESH_RATE, TimeUnit.MILLISECONDS);
+
+        }
+
+        private boolean moveWhileOnScreen() {
+            // Move the BubbleView
+            // Returns true if the BubbleView has exited the screen
+
+            mXPos += mDx;
+            mYPos += mDy;
+            if(isOutOfView()){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        private boolean isOutOfView() {
+            if(mXPos < 0 - mScaledBitmapWidth || mXPos > mDisplayWidth + mScaledBitmapWidth || mYPos < 0- mScaledBitmapWidth || mYPos > mDisplayHeight +mScaledBitmapWidth){
+                return true;
+            }else{
+                return false;
+            }
         }
 
         public void deflect(float velocityX, float velocityY) {
-
+            mDx = velocityX/ REFRESH_RATE;
+            mDy = velocityY/ REFRESH_RATE;
         }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            // Draw the Bubble at its current location
+
+            canvas.save();
+            mRotate +=mDRotate;
+            canvas.rotate(mRotate, mXPos +mScaledBitmapWidth/2, mYPos + mScaledBitmapWidth/2);
+            canvas.drawBitmap(mScaledBitmap, mXPos, mYPos, mPaint);
+            canvas.restore();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_STILL:
+                speedMode = STILL;
+                return true;
+            case MENU_SINGLE_SPEED:
+                speedMode = SINGLE;
+                return true;
+            case MENU_RANDOM_SPEED:
+                speedMode = RANDOM;
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        menu.add(Menu.NONE, MENU_STILL, Menu.NONE, "Still Mode");
+        menu.add(Menu.NONE, MENU_SINGLE_SPEED, Menu.NONE, "Single Speed Mode");
+        menu.add(Menu.NONE, MENU_RANDOM_SPEED, Menu.NONE, "Random Speed Mode");
+
+        return true;
     }
 }
